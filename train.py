447b480150a2c6f4604c065836d1a384a60ff179
asdasd <asdasd>
2023-05-29 15:26:48 +0800

    Change optimizer of standard fine-tune into AdamW

--- train.py
+++ train.py
@@ -159,7 +159,7 @@ def main(args):
                                 {'params':other_learnable_parameters, 'lr':args.lr, 'betas':tuple([args.beta_1, args.beta_2]), 'eps':args.eps, 'weight_decay':args.weight_decay}])
         #Params used from paper, the lr is smaller, more safe for fine tuning to new dataset
     else:
-        optimizer = optim.Adam(model.parameters(), lr=args.lr, betas=(args.beta_1, args.beta_2), eps=args.eps, weight_decay=args.weight_decay)
+        optimizer = optim.AdamW(model.parameters(), lr=args.lr, betas=(args.beta_1, args.beta_2), eps=args.eps, weight_decay=args.weight_decay)
         #Params used from paper, the lr is smaller, more safe for fine tuning to new dataset
 
     # add your own code to track the training progress.
